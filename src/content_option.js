ef631623a5e69b12ed1b0916e620739b052447a0
AlfonsopechMag <97648088+AlfonsopechMag@users.noreply.github.com>
2023-08-23 17:33:17 -0500

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,116 @@
+const logotext = "AP";
+const meta = {
+    title: "Portfolio Alfonso Pech",
+    description: "I’m Alfonso Pech _ Front end developer,currently working in México",
+};
+
+const introdata = {
+    title: "I’m Alfonso Pech",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop web pages",
+    },
+   /*  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum", */
+    your_img_url: "https://dm0qx8t0i9gc9.cloudfront.net/thumbnails/video/BXJT8TRDeiymbx54w/videoblocks-hacker-code-running-down-a-computer-screen-termina_hszutofgp_thumbnail-1080_01.png",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "Enthusiastic web developer eager to contribute to the team's success through hard work, attention to detail, and excellent organizational skills. Clear understanding for analysis and development for user requirements. Motivated to learn, grow and excel.",
+};
+const worktimeline = [
+    {
+        jobtitle: "Front End Developer",
+        where: "Baufest Ingeniería",
+        date: "2023",
+    },
+    {
+        jobtitle: "Front End Developer",
+        where: "Palace Resorts",
+        date: "2019",
+    },    
+    {
+        jobtitle: "Developer/UI",
+        where: "Cupecomi",
+        date: "2018",
+    },
+];
+
+const skills = [
+    {
+        name: "JavaScript",
+        value: 90,
+    },
+    {
+        name: "HTML/CSS/SASS",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "TypeScript",
+        value: 75,
+    },
+    
+    {
+        name: "NodeJS",
+        value: 70,
+    },
+];
+
+const services = [
+    {
+        title: "Front End Development",
+        /* description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.", */
+    },
+    {
+        title: "Wordpress Design",
+        /* description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.", */
+    },
+];
+
+const dataportfolio = [
+    {
+        img: "https://mma.prnewswire.com/media/900516/Palace_Resorts_Logo.jpg?p=facebook",
+        description: "Search engine for reservations in the commercial chain of Palace Resorts.",
+        link: "https://onlinebookingspr.palaceresorts.com/",
+    },
+    {
+        img: "https://mma.prnewswire.com/media/900516/Palace_Resorts_Logo.jpg?p=facebook",
+        description: "Site for external agents of Palace Resorts, where you can generate group/wedding reservations as well as individual reservations and check availability.",
+        link: "https://agencydirect.palaceresorts.com/",
+    },
+    {
+        img: "https://nera-agro.com/img/nera-logo.png",
+        description: "Digital platform that optimizes the financial management of agriculture and makes it simple, practical and integrated.",
+        link: "https://productor.nera-agro.com/",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "alfonsoisup@gmail.com",
+    YOUR_FONE: "+52 (998)301-5610",
+    /* description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ", */
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    linkedin: "https://www.linkedin.com/in/alfonso-pech-88ba311a2/",
+    /* facebook: "https://facebook.com", */    
+    /* twitter: "https://twitter.com", */
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
